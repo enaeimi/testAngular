8e6a608f7ffb8d30336db2a6811123354e5a7874
Ebrahim NAEIMI <ebrahim.naeimi@masterk.com>
2025-04-28 17:56:06 +0200

    Add jQuery and Lodash dependencies; update main.ts with simple arithmetic

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,10 @@ import { AppComponent } from './app/app.component';
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+var x = 10;
+var y = 20;
+var z = x + y;
+console.log(z);
+
+
+
